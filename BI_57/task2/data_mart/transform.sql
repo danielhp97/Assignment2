2554e09cdb47d1f3fd320c4f22b451a0c788a30f
danielhp97 <danielhp1234@gmail.com>
2021-12-10 14:47:45 +0100

    coutnry

--- BI_57/task2/data_mart/transform.sql
+++ BI_57/task2/data_mart/transform.sql
@@ -1,7 +1,13 @@
 /* scripts transformation */
 
 /*INSERT INTO Dim_Customer
-(CustomerKey, AccountNumber, FullName, Gender, Age)*/
+(CustomerKey, AccountNumber, FullName, Gender, Age)
 SELECT C.CustomerID, C.CustomerID, CONCAT(P.FirstName,P.MiddleName,P.LastName), P.Gender, TIMESTAMPDIFF(year,P.BirthDate,STR_TO_DATE('13-09-2021','%d-%m-%Y'))
 FROM TB_Customer as C
 INNER JOIN TB_Person as P on C.PersonID=P.PersonID
+*/
+/* Dim_Location*/
+/* Location Key, Country, Region, Tax Rate*/
+
+SELECT C.CountryID, C.Country, C.Region, C.TaxRate
+FROM TB_Country as C
