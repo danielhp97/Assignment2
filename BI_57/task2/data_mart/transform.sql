c95c451e353fb66f8cdc1d9e978f529de3937d70
danielhp97 <danielhp1234@gmail.com>
2021-12-10 15:05:55 +0100

    dim date

--- BI_57/task2/data_mart/transform.sql
+++ BI_57/task2/data_mart/transform.sql
@@ -7,7 +7,16 @@ FROM TB_Customer as C
 INNER JOIN TB_Person as P on C.PersonID=P.PersonID
 */
 /* Dim_Location*/
-/* Location Key, Country, Region, Tax Rate*/
-
+/* Location Key, Country, Region, Tax Rate
 SELECT C.CountryID, C.Country, C.Region, C.TaxRate
 FROM TB_Country as C
+*/
+
+
+/*Dim_date*/
+SELECT YEAR(OrderDate)*10000 + MONTH(OrderDate)*100 + DAYOFMONTH(OrderDate) as DateKey, ROW_NUMBER() OVER( ORDER BY OrderDate ), WEEKDAY(OrderDate), DAYNAME(OrderDate)
+,DAYOFMONTH(OrderDate), DAYOFYEAR(OrderDate),
+WEEKOFYEAR(OrderDate), MONTHNAME(OrderDate),
+MONTH(OrderDate), QUARTER(OrderDate), YEAR(OrderDate)
+FROM TB_SalesOrderHeader as S
+LIMIT 10
