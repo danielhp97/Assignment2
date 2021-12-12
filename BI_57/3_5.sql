47ac25f4a3126a6bb97d09daeaf1d4e41df3c819
maurizio-papa <73225821+maurizio-papa@users.noreply.github.com>
2021-12-12 20:40:13 +0100

    Update 3_5.sql

--- BI_57/3_5.sql
+++ BI_57/3_5.sql
@@ -1,6 +1,6 @@
-SELECT L.Country as Country , D.CalendarYear as Year , D.EnglishMonthName as Month, F.OrderLineShippingCost as Shipping_Cost  
+SELECT L.Country as Country , D.CalendarYear as Year , D.EnglishMonthName as Month, sum(F.OrderLineShippingCost) as Shipping_Cost  
 FROM Fact_InternetSales as F
 LEFT JOIN Dim_Location as L on L.LocationKey=F.ShipToLocationKey
 LEFT JOIN Dim_Date as D on D.DateKey=F.ShipDateKey
-WHERE L.Country="United Kingdom" AND F.ShipMethod = "Cargo International" AND D.MonthNumberOfYear>0 AND D.MonthNumberOfYear<=12 AND D.CalendarYear=2020
+WHERE L.Country="United Kingdom" AND F.ShipMethod = "Cargo International" AND D.MonthNumberOfYear>0 AND D.MonthNumberOfYear<=6 AND D.CalendarYear=2020
 GROUP BY D.EnglishMonthName
